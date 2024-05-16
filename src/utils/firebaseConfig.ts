fd9681721922067a42ad3a0575393d6e8241049f
darshan <darshanjain768@gmail.com>
2024-05-16 12:49:15 +0530

    All done deployed at firebase

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKGv92-AHA0CcUPhXhJcXcq2YpEG4l4AI",
-  authDomain: "pokedex-dj.firebaseapp.com",
-  projectId: "pokedex-dj",
-  storageBucket: "pokedex-dj.appspot.com",
-  messagingSenderId: "187193344175",
-  appId: "1:187193344175:web:a5204cd7499584e7bad45a",
-  measurementId: "G-QSV1KH9Z4Z"
+  apiKey: "AIzaSyB8d989QesYsIbEmRZsykxCxmYk9vnYANE",
+  authDomain: "dj-pokedex.firebaseapp.com",
+  projectId: "dj-pokedex",
+  storageBucket: "dj-pokedex.appspot.com",
+  messagingSenderId: "488337720949",
+  appId: "1:488337720949:web:648b3193d8ad4829f31a3c",
+  measurementId: "G-5LDKLXF8B0"
 };
 
 const app = initializeApp(firebaseConfig);
